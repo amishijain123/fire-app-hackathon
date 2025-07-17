66e05e931002ac36d35168b1d5d3beb01603d123
Devesh <deveshchandra187@gmail.com>
2025-07-17 15:47:48 +0530

    setup

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/api/ping", methods=["GET"])
+def ping():
+    return jsonify({"message": "pong"}), 200
+
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
