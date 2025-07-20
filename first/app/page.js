2e40e5b9e6b0536e117abdd3f01b987da3a9494c
amishijain123 <jainamishi2005@gmail.com>
2025-07-20 15:32:36 +0530

    Updated homepage and project structure

---
+++ first/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const page = () => {
+  return (
+    <div>
+      <div>
+        i am homepage
+      </div>
+    </div>
+  )
+}
+
+export default page
---
+++ first/first_typescript/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <div>
+            HOMEPAGE
+        </div>
+    </div>
+  )
+}
+
+export default page
